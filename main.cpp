a293a70d8bd2f8ba93abe60c93367d599912f6a8
k022g0072@g.neec.ac.jp <k022g0072@g.neec.ac.jp>
2023-04-28 10:39:15 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <Novice.h>
-
+#include"Matrix4x4.h"
 const char kWindowTitle[] = "LD2A_08_ツクイナチ";
 
+static const int kRowHeight = 20;
+Vector3 translate{ 4.1f,2.6f,0.8f };
+Vector3 scale{ 1.5f,5.2f,7.3f };
+Matrix4x4 translateMatrix = Matrix4x4Ex::MakeTranslateMatrix(translate);
+Matrix4x4 scaleMatrix = Matrix4x4Ex::MakeScaleMatrix(scale);
+Vector3 point{ 2.3f,3.8f,1.4f };
+Matrix4x4 transformMatrix = {
+	1.0f,2.0f,3.0f,4.0f,
+	3.0f,1.0f,1.0f,2.0f,
+	1.0f,4.0f,2.0f,3.0f,
+	2.0f,2.0f,1.0f,3.0f
+};
+Vector3 transformed = Matrix4x4Ex::Transform(point, transformMatrix);
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -32,6 +45,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+		Matrix4x4Ex::VectorScreenPrintf(0, 0, transformed, "transformd");
+		Matrix4x4Ex::MatrixScreenPrintf(0, 0, translateMatrix, "translateMatrix");
+		Matrix4x4Ex::MatrixScreenPrintf(0,kRowHeight*5,scaleMatrix,"scaleMatrix");
 
 		///
 		/// ↑描画処理ここまで
